ad69019ae3f6e957e0b528ed614e43878820ebc2
satyakusuma <satyategar25@gmail.com>
2024-10-31 07:48:47 +0700

    Add Navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import {RouterProvider} from 'react-router-dom'
 import router from './routers/router.jsx'
+import 'remixicon/fonts/remixicon.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
