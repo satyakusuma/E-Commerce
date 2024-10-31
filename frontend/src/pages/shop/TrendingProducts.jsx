b6e37a2ed1494e26be00b08abb4282bf198db386
satyakusuma <satyategar25@gmail.com>
2024-10-31 14:24:11 +0700

    Trending Products Done

--- frontend/src/pages/shop/TrendingProducts.jsx
+++ frontend/src/pages/shop/TrendingProducts.jsx
@@ -17,7 +17,18 @@ const TrendingProducts = () => {
         Collection of Trending Women's Fashion Products.
       </p>
       {/* Product Cards */}
-      <ProductCards products={products}/>
+      <div className="mt-12">
+        <ProductCards products={products.slice(0, visibleProducts)} />
+      </div>
+      
+      {/* load more product button */}
+      <div className="product__btn">
+        {
+            visibleProducts < products.length && (
+                <button className="btn" onClick={loadMoreProducts}>Load More</button>
+            )
+        }
+      </div>
     </section>
   );
 };
