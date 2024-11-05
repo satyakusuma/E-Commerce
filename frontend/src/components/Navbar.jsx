3d421f396d258b4f92f16afef3faceb2e4a5615b
satyakusuma <satyategar25@gmail.com>
2024-11-05 07:10:51 +0700

    Redux Setup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const products = useSelector((state) => state.cart.products);
+
   return (
     <header className='fixed-nav-bar w-nav'>
         <nav className='max-w-screen-2xl mx-auto px-4 flex justify-between items-center'>
@@ -27,7 +31,7 @@ const Navbar = () => {
                 <span>
                     <button className='hover:text-primary'>
                         <i className="ri-shopping-bag-line"></i>
-                        <sup className='text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center'>0</sup>
+                        <sup className='text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center'>{products.length}</sup>
                     </button>
                 </span>
                 <span>
--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import RatingStars from "../../components/RatingStars"
+import { useDispatch } from "react-redux"
+import { addToCart } from "../../redux/features/cart/cartSlice"
 
 const ProductCards = ({ products }) => {
     //console.log(products)
+    const dispatch = useDispatch();
+
+    const handleAddtoCart = (product) => {
+        dispatch(addToCart(product))
+    }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       {
@@ -18,7 +25,12 @@ const ProductCards = ({ products }) => {
                     />
                     </Link>
                     <div className="hover:block absolute top-3 right-3">
-                        <button>
+                        <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleAddtoCart(product)
+                        }}
+                        >
                         <i className="ri-shopping-cart-2-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i>
                         </button>
                     </div>
